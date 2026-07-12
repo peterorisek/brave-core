6a72e83057f2f5260928893a14f44eac9a7630bf
Emerick Rogul <erogul@brave.com>
2026-07-12 11:19:30 -0400

    [cr152] Update horizontal tab strip override
    Upstream moved the horizontal tab strip's construction into a new
    `CreateHorizontalTabStripRegionView()` factory function. Updated our override to
    wrap the new factory function instead, preserving
    `BraveHorizontalTabStripRegionView` for the default (non-unified) tab strip path
    while still deferring to upstream's own logic for the new unified path.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/6feebf41ffa03e5a38fc86f1ccc69a9f5b0aef5b
    
    commit 6feebf41ffa03e5a38fc86f1ccc69a9f5b0aef5b
    Author: Caroline Rising <corising@chromium.org>
    Date:   Mon Jul 6 12:24:59 2026 -0700
    
        [Unified tabstrip] Use BaseTabStripRegionView for the horizontal tabstrip.
    
        This updates the horizontal tab strip to create a new tabstripview using
        the TabCollectionNode system behind the feature flag. This will look
        pretty broken with the flag enabled until all child view orientations
        are updated.
    
        NO_IFTTT=changed class of horizontal region view in both places.
    
        Bug: 523325401
        Change-Id: I8501a572009e63eafd2517db1736496f52217108
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7963961
        Reviewed-by: Vince Lugli <lugli@google.com>
        Commit-Queue: Caroline Rising <corising@chromium.org>
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1657377}

--- chromium_src/chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.cc
+++ chromium_src/chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.cc
@@ -5,9 +5,25 @@
 
 #include "chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.h"
 
+#include "brave/browser/ui/views/frame/brave_tab_strip_region_view.h"
 #include "brave/browser/ui/views/tabs/brave_browser_tab_strip_controller.h"
 #include "brave/browser/ui/views/tabs/brave_new_tab_button.h"
 #include "brave/browser/ui/views/tabs/brave_tab_hover_card_controller.h"
 #include "brave/browser/ui/views/tabs/brave_tab_strip.h"
 
+#define CreateHorizontalTabStripRegionView \
+  CreateHorizontalTabStripRegionView_ChromiumImpl
+
 #include <chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.cc>
+
+#undef CreateHorizontalTabStripRegionView
+
+// We don't support the unified tab strip yet, but otherwise we do want to
+// return our subclass
+std::unique_ptr<TabStripRegionView> CreateHorizontalTabStripRegionView(
+    BrowserView* browser_view) {
+  if (base::FeatureList::IsEnabled(tabs::kTabStripUnification)) {
+    return CreateHorizontalTabStripRegionView_ChromiumImpl(browser_view);
+  }
+  return std::make_unique<BraveHorizontalTabStripRegionView>(browser_view);
+}
